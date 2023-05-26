ff685fa9cd87ad1acf7853a2b1644f8d63dacac5
Hubert <h.kwiatkowski@hotmail.com>
2023-05-26 20:26:23 +0200

    chore: months browsing

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,52 @@
-import './App.css'
+import "./App.css";
+import { format, startOfToday, parse, add } from "date-fns";
+import { ArrowLeft, ArrowRight } from "./icons/index.js";
+import { useState } from "react";
 
 function App() {
+  let today = startOfToday();
+  let [currentMonth, setCurrentMonth] = useState(format(today, "MMMM-yyyy"));
+  let firstDayCurrentMonth = parse(currentMonth, "MMMM-yyyy", new Date());
+  let firstDayPrevMonth = add(firstDayCurrentMonth, { months: -1 });
+  let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
+
+  const prevMonth = () => {
+    setCurrentMonth(format(firstDayPrevMonth, "MMMM-yyyy"))
+  }
+
+  const nextMonth = () => {
+    setCurrentMonth(format(firstDayNextMonth, "MMMM-yyyy"))
+  }
+
   return (
     <div className="App">
-      <h1>Fancy Calendar</h1>
+      <div className="calendar">
+        <div className="select-date">
+          <div className="select">
+            <p>Select date</p>
+            <div></div>
+          </div>
+          <div className="date">Pick date(s)</div>
+        </div>
+
+        <div className="calendar-card">
+          <div className="calendar-months">
+            <div className="prev" onClick={prevMonth}>
+              <ArrowLeft />
+              {format(firstDayPrevMonth, "MMMM")}
+            </div>
+            <div className="current-month">{format(firstDayCurrentMonth, "MMMM yyyy")}</div>
+            <div className="next" onClick={nextMonth}>
+              {format(firstDayNextMonth, "MMMM")}
+              <ArrowRight />
+            </div>
+          </div>
+          <div className="calendar-week-days"></div>
+          <div className="calendar-month-days"></div>
+        </div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
---
+++ src/icons/index.js
@@ -0,0 +1,4 @@
+import { ArrowLeft } from "./ArrowLeft/ArrowLeft.jsx";
+import { ArrowRight } from "./ArrowRight/ArrowRight.jsx";
+
+export { ArrowLeft, ArrowRight };
