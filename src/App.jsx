188bdc60d267c82ecc0572c5b42e5f92ed0c79aa
Hubert <h.kwiatkowski@hotmail.com>
2023-05-26 20:04:32 +0200

    chore: months browsing

--- src/App.jsx
+++ src/App.jsx
@@ -7,8 +7,16 @@ function App() {
   let today = startOfToday();
   let [currentMonth, setCurrentMonth] = useState(format(today, "MMMM-yyyy"));
   let firstDayCurrentMonth = parse(currentMonth, "MMMM-yyyy", new Date());
-  let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
   let firstDayPrevMonth = add(firstDayCurrentMonth, { months: -1 });
+  let firstDayNextMonth = add(firstDayCurrentMonth, { months: 1 });
+
+  const prevMonth = () => {
+    setCurrentMonth(format(firstDayPrevMonth, "MMMM-yyyy"))
+  }
+
+  const nextMonth = () => {
+    setCurrentMonth(format(firstDayNextMonth, "MMMM-yyyy"))
+  }
 
   return (
     <div className="App">
@@ -23,14 +31,15 @@ function App() {
 
         <div className="calendar-card">
           <div className="calendar-months">
-            <div className="prev">
+            <div className="prev" onClick={prevMonth}>
               <ArrowLeft />
               {format(firstDayPrevMonth, "MMMM")}
             </div>
-            <div className="current-month">{format(today, "MMMM yyyy")}</div>
-            <div className="next">
+            <div className="current-month">{format(firstDayCurrentMonth, "MMMM yyyy")}</div>
+            <div className="next" onClick={nextMonth}>
               {format(firstDayNextMonth, "MMMM")}
               <ArrowRight />
+
             </div>
           </div>
           <div className="calendar-week-days"></div>
